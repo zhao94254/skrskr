d85a8c0d23955733023c9a2c1148361e65bfd087
zhaopengyun <zhao942548838@qq.com>
2018-12-28 16:52:25 +0800

    添加基本的抽象

--- skrq/base.py
+++ skrq/base.py
@@ -3,3 +3,29 @@
 # @Created on    : 2018/12/28 3:08 PM
 # @Author  : zpy
 # @Software: PyCharm
+
+# 这里主要写Connection & Transport 的抽象
+
+class Connection(object):
+    """
+    提供一层接口，支持多个transport
+    """
+
+    def parse_uri(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class Transport(object):
+    """
+    transport 抽象
+    """
+    pass
+
+
+
---
+++ skrq/pubsub.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 2018/12/28 4:51 PM
+# @Author  : zpy
+# @Software: PyCharm
+
+
+class Publisher(object):
+    pass
+
+
+class Consumer(object):
+    pass
